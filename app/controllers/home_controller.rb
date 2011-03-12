430c81b7c26a574f39299ba3e280ff1b732abcbf
VladimirAtHome <ovcharenkovv@gmail.com>
2011-03-12 10:40:46 +0200

    add yandex metrika, also start carusel at home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @top_products = Product.find_top_products(9)
+    @top_products = Product.find_top_products(12)
     @new_products = Product.find_new_products(6)
     @hot_products = Product.find_hot_products(3)
   end
