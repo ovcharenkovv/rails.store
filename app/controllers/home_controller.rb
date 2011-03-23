3f610efb5cc740e98ef5f093aa8df3aef8b06051
VladimirAtHome <ovcharenkovv@gmail.com>
2011-03-23 21:38:07 +0200

    litle fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-    SitemapGenerator::Generator.run
-
     @top_products = Product.find_top_products(12)
     @new_products = Product.find_new_products(6)
     @hot_products = Product.find_hot_products(3)
