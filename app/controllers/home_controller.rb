b746357745ded343e0df55d44bbcad04b8ede9cd
VladimirAtHome <ovcharenkovv@gmail.com>
2011-03-24 23:02:23 +0200

    fix carusel counts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 class HomeController < ApplicationController
   def index
-#    @top_products = Product.find_top_products(12)
-#    @new_products = Product.find_new_products(14)
-    @top_products = Product.find_top_products(3)
-    @new_products = Product.find_new_products(3)
-
+    @top_products = Product.find_top_products(12)
+    @new_products = Product.find_new_products(14)
     #@hot_products = Product.find_hot_products(16)
     @random_products = Product.find_random_products(20,@top_products+@new_products)
   end
