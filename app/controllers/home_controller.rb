fbea36b27170baf7e06386f3f1964da65b44f609
VladimirAtHome <ovcharenkovv@gmail.com>
2011-03-24 19:21:31 +0200

    fix repetititon products in see also, fix frontpage carusel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @top_products = Product.find_top_products(12)
-    @new_products = Product.find_new_products(6)
+    @top_products = Product.find_top_products(18)
+    @new_products = Product.find_new_products(18)
     @hot_products = Product.find_hot_products(3)
   end
 end
