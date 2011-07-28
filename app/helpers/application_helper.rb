825acb1249459ec30450122ecd270f1b6b218757
Vladimir <ovcharenkovv@gmail.com>
2011-07-28 15:44:22 +0300

    fix content header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,7 +31,7 @@ module ApplicationHelper
       if param[:id] && !param[:cart_id]
         ret += Product.find(param[:id]).title.to_s
         ret += ' - '
-        ret += Product.find(param[:id]).price.to_s+'грн.'
+        ret += Product.find(param[:id]).price.to_s+' грн.'
         ret += ' | '
       end
     end
@@ -43,7 +43,23 @@ module ApplicationHelper
       ret += Author.find(param[:author_id]).name.to_s
       ret += ' | '
     end
-    ret += 'Магазин изделий ручной работы, ручная работа, handmade, рукоделие, поделки, украшения, ексклюзив, хендмейд'
+
+    if param[:author_id]
+      ret += Author.find(param[:author_id]).name.to_s
+      ret += ' | '
+    end
+
+    if param[:post_slug]
+      ret += Post.find_by_slug!(params[:post_slug]).title.to_s
+      ret += ' | '
+    end
+
+    if param[:post_category_slug]
+      ret += PostCategory.find_by_slug!(params[:post_category_slug]).name.to_s
+      ret += ' | '
+    end
+
+    ret += 'Магазин изделий ручной работы hand made'
 
     ret
   end
