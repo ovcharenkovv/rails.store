797330ddac74b10aa486d558880d314d92f01a17
VladimirAtHome <ovcharenkovv@gmail.com>
2011-08-12 16:18:18 +0300

    del old migration

--- db/migrate/20110204101618_add_author_id_to_custom_orders.rb
+++
@@ -1,9 +0,0 @@
-class AddAuthorIdToCustomOrders < ActiveRecord::Migration
-  def self.up
-    add_column :custom_orders, :author_id, :integer
-  end
-
-  def self.down
-    remove_column :custom_orders, :author_id
-  end
-end
