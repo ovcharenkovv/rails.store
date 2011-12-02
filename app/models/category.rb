91ee837f734652fe6a36edf6960b0d2525965fe0
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2011-12-02 18:13:00 +0200

    add edit button on show view

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,4 +10,25 @@ class Category < ActiveRecord::Base
   scope :non_root_category, :conditions => "parent_id IS NOT NULL", :order => "ordering"
   #scope :in_category, lambda {|c| {:include => [:category], :conditions => ["categories.id = ? OR categories.parent_id = ?", c, c]} }
 
+  def all_children
+    all = []
+    self.children.each do |category|
+      all << category
+      root_children = category.all_children.flatten
+      all << root_children unless root_children.empty?
+    end
+    return all.flatten
+  end
+
+  def self.without_children
+    all = []
+    find(:all,:order=>"ordering").each do |category|
+      if category.children.count <=1
+        all << category
+      end
+    end
+    all
+  end
+
+
 end
