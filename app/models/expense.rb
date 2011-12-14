be1d15c48966861ac36b07c83f90658989e7e294
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2011-12-14 10:42:54 +0200

    fix get expenses sum

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ActiveRecord::Base
   def self.for_month month
-    find(:all, :conditions => [" MONTH(created_at) between ? AND ?", month-1, month])
+    find(:all, :conditions => [" MONTH(created_at) = ?", month])
   end
 end
