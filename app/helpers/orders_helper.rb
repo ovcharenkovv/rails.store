daabc8869b8e8e7ceeae27d86212c5b0f57b927c
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2011-12-18 15:07:35 +0200

    backend order management chenge

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,14 @@
 module OrdersHelper
+  def priority_class order_date, status
+
+    if status=='success'
+      "green"
+    else
+      if DateTime.now.to_date.to_date >= order_date.to_date+4
+        "urgent"
+      elsif DateTime.now.to_date.to_date >= order_date.to_date+2
+        "major"
+      end
+    end
+  end
 end
