105f9422d8def0c753bb484e119f70b098b827b9
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-01-23 21:49:24 +0200

    fresh_when refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,10 @@ class PostsController < ApplicationController
       return
     end
     @posts = @post_category.posts.find(:all ,:order=>'created_at desc').paginate :page=>params[:page], :per_page => '5'
-    fresh_when(:etag => @posts)
+    fresh_when(:etag => [@posts,@current_cart.line_items.count])
   end
   def show
     @post = @post_category.posts.find_by_slug!(params[:post_slug])
-    fresh_when :last_modified => @post.updated_at.utc
+    fresh_when(:etag => [@post,@current_cart.line_items.count])
   end
 end
