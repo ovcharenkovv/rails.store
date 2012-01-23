e2b107f64a4ab877aa36a7e848a9640a318d382c
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-01-23 11:04:01 +0200

    tuning product page caching

--- app/sweepers/product_sweeper.rb
+++ app/sweepers/product_sweeper.rb
@@ -16,7 +16,16 @@ class ProductSweeper < ActionController::Caching::Sweeper
   private
 
   def expire_cache_for(product)
-    #expire_fragment("product_#{product.category.id}_#{product.id}")
+    expire_fragment("product_#{product.category.id}_#{product.id}")
+
+    if previous_product = Product.previous_product(product.id,product.category_id)
+      expire_fragment("product_#{previous_product.category.id}_#{previous_product.id}")
+    end
+
+    if next_product = Product.next_product(product.id,product.category_id)
+      expire_fragment("product_#{next_product.category.id}_#{next_product.id}")
+    end
+
     expire_fragment("category#{product.category.id}_author#{}_page#{}_sort#{}")
     expire_fragment("category#{}_author#{product.author.id}_page#{}_sort#{}")
     expire_fragment(:controller => "products", :action => "index")
