3bc5039bcd4f4c288598a34141af1dfa3f6d50ab
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-01-23 17:46:30 +0200

    add etag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,22 +13,25 @@ class PostsController < ApplicationController
       redirect_to root_url
       return
     end
-    @posts = @post_category.posts.find(:all ,:order=>'created_at desc').paginate :page=>params[:page], :per_page => '3'
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @posts }
-    end
+    @posts = @post_category.posts.find(:all ,:order=>'created_at desc').paginate :page=>params[:page], :per_page => '5'
+    fresh_when(:etag => @posts)
+    #fresh_when :last_modified => @posts.updated_at.utc
+    #respond_to do |format|
+    #  format.html # index.html.erb
+    #  format.xml  { render :xml => @posts }
+    #end
   end
 
   # GET /posts/1
   # GET /posts/1.xml
   def show
     @post = @post_category.posts.find_by_slug!(params[:post_slug])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @post }
-    end
+    fresh_when :last_modified => @post.updated_at.utc
+    #
+    #respond_to do |format|
+    #  format.html # show.html.erb
+    #  format.xml  { render :xml => @post }
+    #end
   end
 
 end
