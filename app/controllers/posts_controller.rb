8b5d04cd7bfdc503fa105348ef768d9ad182800f
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-01-24 09:50:46 +0200

    fix fresh

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,10 @@ class PostsController < ApplicationController
       return
     end
     @posts = @post_category.posts.find(:all ,:order=>'created_at desc').paginate :page=>params[:page], :per_page => '5'
-    fresh_when(:etag => [@posts,@current_cart.line_items.count])
+    fresh_when(:etag => [@posts,@current_cart,@current_cart.line_items])
   end
   def show
     @post = @post_category.posts.find_by_slug!(params[:post_slug])
-    fresh_when(:etag => [@post,@current_cart.line_items.count])
+    fresh_when(:etag => [@post,@current_cart,@current_cart.line_items])
   end
 end
