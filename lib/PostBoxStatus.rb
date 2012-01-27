3cd8fe70fdffe4b767c47448e3b6247344ce6191
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-01-27 12:07:56 +0200

    inc font size next prev and? remove iconv support

--- lib/PostBoxStatus.rb
+++ lib/PostBoxStatus.rb
@@ -1,11 +1,8 @@
 # -*- encoding : utf-8 -*-
 module PostBoxStatus
-
   def PostBoxStatus.info post_id, post_type
     require 'net/http'
     require 'uri'
-    require 'iconv'
-
     if post_type == 'Укрпочта - 20 грн.'
       page = Hpricot(Net::HTTP.get(URI.parse("http://80.91.187.254:8080/servlet/SMCSearch2?lang=ua&barcode=#{post_id}".strip)))
       response = page.search("//center//div//div") if !page.nil?
@@ -18,7 +15,9 @@ module PostBoxStatus
     if post_type == 'Новая почта - 20 грн.'
       page = Hpricot(Net::HTTP.get(URI.parse("http://novaposhta.ua/frontend/tracking?en=#{post_id}".strip)))
       response = page.search("//td[@class='stdText']//span") if !page.nil?
-      result = Iconv.conv("UTF8", "CP1251", response.to_s)
+      #result = Iconv.conv("UTF8", "CP1251", response.to_s)
+      ec = Encoding::Converter.new "Windows-1251","UTF-8",:invalid=>:replace,:undef=>:replace,:replace=>""
+      result = ec.convert response.to_s
     end
     result
   end
