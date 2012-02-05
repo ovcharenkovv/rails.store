9d286afec63dc70cc58f1ef4e80fb4f3018c4054
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-02-05 19:05:04 +0200

    novaposhta change format

--- lib/PostBoxStatus.rb
+++ lib/PostBoxStatus.rb
@@ -15,7 +15,7 @@ module PostBoxStatus
 
     if post_type == 'Новая почта - 20 грн.'
       page = Hpricot(Net::HTTP.get(URI.parse("http://novaposhta.ua/frontend/tracking?en=#{post_id}".strip)))
-      response = page.search("//td[@class='stdText']//span") if !page.nil?
+      response = page.search("//div[@id='main_content']//table//td[@class='stdText']") if !page.nil?
       result = Iconv.conv("UTF8", "CP1251", response.to_s)
       #ec = Encoding::Converter.new "Windows-1251","UTF-8",:invalid=>:replace,:undef=>:replace,:replace=>""
       #result = ec.convert response.to_s
