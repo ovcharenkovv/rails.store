3e9b60500b7f253e370ffe6df4e0ba95fdeef990
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-03-01 22:01:59 +0200

    try catch on post box bed response

--- lib/PostBoxStatus.rb
+++ lib/PostBoxStatus.rb
@@ -4,21 +4,25 @@ module PostBoxStatus
     require 'net/http'
     require 'uri'
     require 'iconv'
-    if post_type == 'Укрпочта - 20 грн.'
-      page = Hpricot(Net::HTTP.get(URI.parse("http://80.91.187.254:8080/servlet/SMCSearch2?lang=ua&barcode=#{post_id}".strip)))
-      response = page.search("//center//div//div") if !page.nil?
-      result = String.new
-      for i in 1..2
-        result+="<p style='color:red;'>#{response[i].inner_html}</p>" if !response[i].nil?
-      end
-    end
+    begin
+      if post_type == 'Укрпочта - 20 грн.'
 
-    if post_type == 'Новая почта - 20 грн.'
-      page = Hpricot(Net::HTTP.get(URI.parse("http://novaposhta.ua/frontend/tracking?en=#{post_id}".strip)))
-      response = page.search("//div[@id='main_content']//table//td[@class='stdText']") if !page.nil?
-      result = Iconv.conv("UTF8", "CP1251", response.to_s)
-      #ec = Encoding::Converter.new "Windows-1251","UTF-8",:invalid=>:replace,:undef=>:replace,:replace=>""
-      #result = ec.convert response.to_s
+        page = Hpricot(Net::HTTP.get(URI.parse("http://80.91.187.254:8080/servlet/SMCSearch2?lang=ua&barcode=#{post_id}".strip)))
+        response = page.search("//center//div//div") if !page.nil?
+        result = String.new
+        for i in 1..2
+          result+="<p style='color:red;'>#{response[i].inner_html}</p>" if !response[i].nil?
+        end
+      end
+
+      if post_type == 'Новая почта - 20 грн.'
+        page = Hpricot(Net::HTTP.get(URI.parse("http://novaposhta.ua/frontend/tracking?en=#{post_id}".strip)))
+        response = page.search("//div[@id='main_content']//table//td[@class='stdText']") if !page.nil?
+        result = Iconv.conv("UTF8", "CP1251", response.to_s)
+        #ec = Encoding::Converter.new "Windows-1251","UTF-8",:invalid=>:replace,:undef=>:replace,:replace=>""
+        #result = ec.convert response.to_s
+      end
+    rescue Exception => result
     end
     result
   end
