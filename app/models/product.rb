163e381f586dba71fa6665772c9a4d0dd285e657
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-07-03 23:52:39 +0300

    add search feature on the product index page

--- app/models/product.rb
+++ app/models/product.rb
@@ -53,7 +53,7 @@ class Product < ActiveRecord::Base
 
   def self.search(params)
     if params[:search]
-      where('title LIKE ?', "%#{params[:search]}%").where('published is true').paginate :page => params[:page], :order => 'created_at desc', :per_page => 60
+      where('title LIKE ?', "%#{params[:search]}%").where('published is true').paginate :page => params[:page], :order => 'created_at desc', :per_page => 33
     else
       nil
     end
