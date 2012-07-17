ba096cb454649f4e241ddbee40b9f87b3af387b1
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-07-17 23:52:31 +0300

    SEO fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,24 @@
 # -*- encoding : utf-8 -*-
+
 module ApplicationHelper
   def meta_title(param)
     ret=""
+
+    if param[:controller]=='home'
+      ret += 'PoshStore - интернет магазин изделий ручной работы, бижутериии, авторских работ, хенд мейд, хендмейд, handmade.'
+    end
+
     if param[:controller]=='products' && param[:action]=='show'
       if param[:id] && !param[:cart_id]
         ret += Product.find(param[:id]).title.to_s
-        ret += ' - '
-        ret += Product.find(param[:id]).price.to_s+' грн.'
         ret += ' '
         ret += ' Купить в интернет магазине. '
       end
     end
     if param[:category_id] && param[:action]!='show'
-      ret += Category.find(param[:category_id]).name.to_s
-      ret += ' '
+      ret += ' Купить '
+      ret += Category.find(param[:category_id]).name.mb_chars.downcase.to_s
+      ret += ' в интернет магазине.'
     end
     if param[:author_id]
       ret += Author.find(param[:author_id]).name.to_s
@@ -30,7 +35,6 @@ module ApplicationHelper
       ret += ' '
     end
 
-    ret += 'PoshStore - интернет магазин изделий ручной работы, бижутериии, авторских работ, хенд мейд, хендмейд, handmade.'
 
     ret
   end
@@ -38,9 +42,34 @@ module ApplicationHelper
   def meta_keywords(param)
     ret=""
 
+    if param[:controller]=='home'
+      ret += 'купить, купить в интернет магазине, интернет-магазин, ручная работа, бижутерия, hand made, handmade, хенд мейд, аксессуары, полимерная, декупаж'
+    end
+
+    if param[:controller]=='products' && param[:action]=='show'
+      if param[:id] && !param[:cart_id]
+        ret += 'купить '
+        ret += Product.find(param[:id]).title.to_s
+        ret += ', '
+        ret += 'продажа '
+        ret += Product.find(param[:id]).title.to_s
+        ret += ', '
+        ret += Category.find(param[:category_id]).name
+        ret += ' '
+        ret += Product.find(param[:id]).title.to_s
+        ret += ', '
+        ret += 'интернет магазин, доставка '
+      end
+    end
+
     if param[:category_id]
+      ret += 'купить '
       ret += Category.find(param[:category_id]).name
       ret += ', '
+      ret += 'продажа '
+      ret += Category.find(param[:category_id]).name
+      ret += ', '
+      ret += 'интернет магазин, доставка '
     end
 
     if param[:post_slug]
@@ -53,40 +82,33 @@ module ApplicationHelper
       ret += ', '
     end
 
-    if param[:controller]=='products'
-      if param[:id] && !param[:cart_id]
-        ret += Product.find(param[:id]).title
-        ret += ', '
-      end
-    end
-    ret += 'купить, купить в интернет магазине, интернет-магазин, ручная работа, бижутерия, hand made, handmade, хенд мейд, аксессуары, полимерная, декупаж'
-
     ret
   end
 
   def meta_description(param)
     ret=""
     if param[:post_slug]
-      ret += 'Здесь вы найдете изделия ручной работы и мастер-классы по изготовлению | '
+      ret += 'Здесь вы найдете мастер-классы по изготовлению '
       ret += Post.find_by_slug!(params[:post_slug]).title
     else
-      ret += 'Здесь можно купить изделия ручной работы, '
-
-      if param[:category_id]
+      if param[:controller]=='products' && param[:action]=='index'
+        ret += 'Здесь можно купить '
         ret += Category.find(param[:category_id]).name
-        ret += ', '
+        ret += ', хендмейд, handmade, украшения, и многое другое'
       end
 
-      ret += 'хендмейд бижутерия, handmade украшения, открытки ручной работы, '
-
-      if param[:controller]=='products'
+      if param[:controller]=='products' && param[:action]=='show'
         if param[:id] && !param[:cart_id]
-          ret += Product.find(param[:id]).title.to_s
-          ret += ', '
+          product = Product.find(param[:id])
+          if product.description.length > 100
+            ret += strip_tags(Product.find(param[:id]).description.to_s)[0..250]
+          else
+            ret += Category.find(param[:category_id]).name.to_s
+            ret += 'Здесь можно купить '
+            ret += Product.find(param[:id]).title.to_s
+          end
         end
       end
-
-      ret += 'и массу других товаров.'
 
     end
 
