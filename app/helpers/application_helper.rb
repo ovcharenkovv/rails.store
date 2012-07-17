96ed23ff7107c226b0d36b43592d2aab875f318d
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-07-17 08:47:18 +0300

    small SEO fix title description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,34 +7,29 @@ module ApplicationHelper
         ret += Product.find(param[:id]).title.to_s
         ret += ' - '
         ret += Product.find(param[:id]).price.to_s+' грн.'
-        ret += ' | '
+        ret += ' '
       end
     end
     if param[:category_id]
       ret += Category.find(param[:category_id]).name.to_s
-      ret += ' | '
+      ret += ' '
     end
     if param[:author_id]
       ret += Author.find(param[:author_id]).name.to_s
-      ret += ' | '
-    end
-
-    if param[:author_id]
-      ret += Author.find(param[:author_id]).name.to_s
-      ret += ' | '
+      ret += ' '
     end
 
     if param[:post_slug]
       ret += Post.find_by_slug!(params[:post_slug]).title.to_s
-      ret += ' | '
+      ret += ' '
     end
 
     if param[:post_category_slug]
       ret += PostCategory.find_by_slug!(params[:post_category_slug]).name.to_s
-      ret += ' | '
+      ret += ' '
     end
 
-    ret += 'PoshStore - магазин изделий ручной работы, handmade бижутериии, авторские работы.'
+    ret += 'Купить в интернет магазине, PoshStore - магазин изделий ручной работы, handmade бижутериии, авторские работы.'
 
     ret
   end
@@ -63,7 +58,7 @@ module ApplicationHelper
         ret += ', '
       end
     end
-    ret += 'ручная работа, бижутерия, hand made, handmade, хенд мейд, купить, интернет-магазин, полимерная бижутерия, декупаж'
+    ret += 'купить, купить в интернет магазине, интернет-магазин, ручная работа, бижутерия, hand made, handmade, хенд мейд, аксессуары, полимерная, декупаж'
 
     ret
   end
